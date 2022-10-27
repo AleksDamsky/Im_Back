2313082c5fbc2ca81d8a83e55b8879cf800341ca
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-10-27 22:33:22 +0300

    I build new JS file and write there generation colors code

---
+++ vladilenCourse.js
@@ -0,0 +1,20 @@
+const cols = document.querySelectorAll(".col");
+
+function generationRandomColors() {
+  const hexCodes = "0123456789ABCDEF";
+  let colors = "";
+
+  for (let i = 0; i <= 6; i++) {
+    colors += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+
+  return "#" + colors;
+}
+
+function setRandomColors() {
+  cols.forEach((cols) => {
+    console.log(cols);
+  });
+}
+
+setRandomColors();
