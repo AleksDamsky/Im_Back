3ec4077e6caa12b7d4295fd30de254a3db071827
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-10-29 19:47:18 +0300

    vladilen course

--- vladilenCourse.js
+++ vladilenCourse.js
@@ -1,20 +1,31 @@
 const cols = document.querySelectorAll(".col");
 
 function generationRandomColors() {
-  const hexCodes = "0123456789ABCDEF";
+  const hexCodes = "0123456789abcdef";
   let colors = "";
-
   for (let i = 0; i <= 6; i++) {
     colors += hexCodes[Math.floor(Math.random() * hexCodes.length)];
   }
-
   return "#" + colors;
 }
 
+// console.log(generationRandomColors());
+
 function setRandomColors() {
   cols.forEach((col) => {
-    col.style.background = generationRandomColors();
+    const text = document.querySelector("h2");
+    const color = generationRandomColors();
+
+    col.style.backgroundColor = chroma.random();
+    text.textContent = color;
+
+    setTextColors(color, text);
   });
+}
+
+function setTextColors(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > 0.5 ? "black" : "white";
 }
 
 setRandomColors();
