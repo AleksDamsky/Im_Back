594e1bf2c4b7a567824a4edb8c90b2323a9218a6
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-11-03 07:40:13 +0300

    I didn't' find a bug in the code. Calculator is not working

--- calculator/calculatorJS/calcIndex.js
+++ calculator/calculatorJS/calcIndex.js
@@ -5,10 +5,11 @@ let previousOperator; // крайний/последний оператор
 const screen = document.querySelector(".screen"); // здесь мы через документ обращаемся к секции, которая отображает 0 в буффере.
 
 function buttonClick(value) {
-  if (isNan(value)) {
+  if (isNaN(value)) {
+    /*isNan определяет является ли переменная строкой или нет*/
     handleSymbol(value);
   } else {
-    hundleNumber(value);
+    handleNumber(value);
   }
   screen.innerText = buffer; //эта строка у нас считывает цифры, написанные в буфере
 }
@@ -23,16 +24,18 @@ function handleSymbol(symbol) {
       if (previousOperator === null) {
         return;
       }
+      /*Что делает вот эта функция?*/
       flushOperator(parseInt(buffer)); //что такое parseInt? Это функция, которая из строки возвращает целые числа
       previousOperator = null;
       buffer = runningTotal;
       runningTotal = 0;
       break;
+    /*Вот тут заканчивается...*/
     case "←":
       if (buffer.length === 1) {
         buffer = "0";
       } else {
-        buffer.toString(0, buffer.length - 1);
+        buffer.toString(0, buffer.length - 1); // Этот метод возвращает строку вместо числа
       }
       break;
     case "+":
@@ -44,12 +47,13 @@ function handleSymbol(symbol) {
   }
 }
 
+/*Функция, которая возвращает 0, если в буфере ничего не введено (если там нулик)*/
 function handleMath(symbol) {
   if (buffer === "0") {
     return;
   }
 
-  const intBuffer = parseInt(buffer);
+  const intBuffer = parseInt(buffer); //константа, которая из буфера достает целое число
 
   if (runningTotal === 0) {
     runningTotal = intBuffer;
@@ -61,6 +65,7 @@ function handleMath(symbol) {
   buffer = "0";
 }
 
+/*Эта функция у нас обрабатывает буффер и если крайний оператор один из ниже перечисленных, то константа пополнятся новым значением(в зависимости от  + - / *)*/
 function flushOperator(intBuffer) {
   if (previousOperator === "+") {
     runningTotal += intBuffer;
@@ -73,7 +78,7 @@ function flushOperator(intBuffer) {
   }
 }
 
-function hundleNumber(numberString) {
+function handleNumber(numberString) {
   if (buffer === "0") {
     buffer = numberString;
   } else {
@@ -90,3 +95,4 @@ function init() {
 }
 
 init();
+/*Калькулятор не реагирует на кнопки. Ничего не происходит. В консоле тоже ничего не отображается.*/
