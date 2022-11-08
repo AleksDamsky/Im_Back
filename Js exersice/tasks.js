5e4f622ba61d364b16dd0de08d9e8a5108d930a4
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-11-08 15:26:28 +0300

    some practice on JS

--- Js exersice/tasks.js
+++ Js exersice/tasks.js
@@ -1,3 +1,11 @@
-function palindrome(str) {
-  str;
-}
+// function palindrome(str) {
+//   str;
+// }
+
+const btnNear = document.querySelector(".js-btn");
+const taskText = document.querySelector(".tasksDecide");
+const feedback = document.querySelector(".textFromUsers");
+
+btnNear.addEventListener("click", function () {
+  feedback.innerHTML = `<h1>${taskText.value}</h1>`;
+});
