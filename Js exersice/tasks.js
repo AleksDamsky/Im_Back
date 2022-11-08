6bbf7271666ec5409ed4b357dd6c22d36aa77989
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-11-08 16:06:40 +0300

    indulge with js code

--- Js exersice/tasks.js
+++ Js exersice/tasks.js
@@ -6,6 +6,14 @@ const btnNear = document.querySelector(".js-btn");
 const taskText = document.querySelector(".tasksDecide");
 const feedback = document.querySelector(".textFromUsers");
 
+function changeText(taskText) {
+  let newText = toString(taskText);
+}
+
 btnNear.addEventListener("click", function () {
-  feedback.innerHTML = `<h1>${taskText.value}</h1>`;
+  feedback.innerHTML = changeText();
 });
+
+// btnNear.addEventListener("click", function () {
+//   feedback.innerHTML = `<h1>${taskText.value}</h1>`;
+// });
