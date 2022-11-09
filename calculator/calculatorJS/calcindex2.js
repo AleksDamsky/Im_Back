fc6094244aaf318e52b84a8e528780c5967eca5d
Александр Александрович Хрипунов <99286602+AleksDamsky@users.noreply.github.com>
2022-11-09 12:43:56 +0300

    make cod on JS

--- calculator/calculatorJS/calcindex2.js
+++ calculator/calculatorJS/calcindex2.js
@@ -1,8 +1,20 @@
-const btn = document.querySelector(".calc-buttons");
-const Scren = document.querySelector(".screen");
+// const btn = document.querySelector(".calc-buttons");
+// const Scren = document.querySelector(".screen");
 
-let container = btn.value;
+// let container = btn.value;
 
-btn.addEventListener("click", () => {
-  Scren.innerHTML = container;
-});
+// btn.addEventListener("click", () => {
+//   Scren.innerHTML = container;
+// });
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+let getNumber = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid;
+
+  mid = Math.random(left - right);
+};
+
+console.log(getNumber(array, 22));
